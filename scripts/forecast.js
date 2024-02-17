b937811af4f0b0f5616a1fc8523958df06a02e6d
KABANOFESTO <kabanofesto1@gmail.com>
2024-02-17 12:03:36 +0200

    ch-addDom

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -24,9 +24,9 @@ const getCity = async (city) => {
     return data[0];
 
 };
-getCity('manchester').then(data => {
-    return getWeather(data.Key)
-}).then(data => {
-    console.log(data);
-})
-    .catch(err => console.log('rejected', err));
+// getCity('manchester').then(data => {
+//     return getWeather(data.Key)
+// }).then(data => {
+//     console.log(data);
+// })
+//     .catch(err => console.log('rejected', err));
