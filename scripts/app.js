617b809449c911bad25552d8d24aeb88c1e62ec2
KABANOFESTO <kabanofesto1@gmail.com>
2024-02-23 23:29:02 +0200

    ch-addClass

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
     // destructure properties
@@ -24,48 +25,30 @@ const updateUI = (data) => {
 
     let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
 
-
-    // if (weather.IsDayTime) {
-    //     timeSrc = 'img/day.svg';
-    // } else {
-    //     timeSrc = 'img/night.svg';
-    // }
     time.setAttribute('src', timeSrc);
 
-    // remove the d-none class if present
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
 };
 
-const updateCity = async (city) => {
-
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
-    return { cityDets, weather };
-
-};
-
 cityForm.addEventListener('submit', e => {
-    // prevent default action
+
     e.preventDefault();
 
-    // get city value
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    // update the ui with new city
-    updateCity(city)
+    forecast.updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 
-    // set localStorage
     localStorage.setItem('city', city);
 });
 const localCity = localStorage.getItem('city');
 
 if (localCity) {
-    updateCity(localStorage.getItem('city'))
+    forecast.updateCity(localStorage.getItem('city'))
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 }
